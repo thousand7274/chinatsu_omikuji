fff6bcc052ee583750903a2fe21245b9f96e8176
thousand7274 <thousand7274@gmail.com>
2019-09-20 14:33:06 +0900

    仕様変更
    おみくじをボタンを押したらひけるように仕様を変更しました

--- app.js
+++ app.js
@@ -1,32 +1,31 @@
 
+let image = ['img/dai.png', 'img/chu.png', 'img/suekichi.png', 'img/syo.png', 'img/kichi.png', 'img/kyou.png', 'img/daikyo.png'];
+let words = ['大吉！やったー！', '中吉！いい感じ！', '末吉！まぁまぁ？', '小吉！小さいけど吉だよ！', '吉！いいことあるよ！', '凶！！あれまー！何か人にいいことをしよ！', '大凶！！背後に気をつけて！！'];
+document.getElementById('omikujiImg');
+let text = document.getElementById('text');
+let container = document.getElementById('container');
+let drop = document.getElementById('dropOmikuji');
+let btn = document.createElement('button');
 
-
-// let num = Math.floor(Math.random() * omikujiImage.length);
-// console.log(num);
-let omikujiImage = ['img/dai.png','img/chu.png','img/suekichi.png','img/syo.png','img/kichi.png','img/kyou.png','img/daikyo.png'];
-let omikujiWords = ['大吉！やったー！','中吉！いい感じ！','末吉！まぁまぁ？','小吉！小さいけど吉だよ！','吉！いいことあるよ！','凶！！あれまー！何か人にいいことをしよ！','大凶！！背後に気をつけて！！'];
-document.getElementById('clickOmikuji');
-document.getElementById('comments');
 clickOmikuji.addEventListener('click',
-  function omikuji(){
-  let num = Math.floor(Math.random() * omikujiImage.length);
-  clickOmikuji.src = omikujiImage[num];
-  comments.textContent = omikujiWords[num];
-  if(num === 6){
-    document.body.style.backgroundColor = 'red';
-    document.body.style.fontSize = '30px';
+  function omikuji() {
+    let num = Math.floor(Math.random() * image.length);
+    omikujiImg.src = image[num];
+    text.textContent = words[num];
+    if (num === 6) {
 
-  }else if(num === 0){
-    document.body.style.backgroundColor = 'lightpink';
-    document.body.style.color = 'black';
-    document.body.style.fontSize = '30px';
-  }else{
-    document.body.style.backgroundColor = '#1d1d1d';
-    document.body.style.color = 'aliceblue';
-    document.body.style.fontSize = '20px';
-    
-  }
+      document.body.style.backgroundColor = 'red';
+      container.classList.add('kyou');
 
 
+    } else if (num === 0) {
+      document.body.style.backgroundColor = 'lightpink';
+      document.body.style.color = 'black';
+      document.body.style.fontSize = '30px';
+    } else {
+      document.body.style.backgroundColor = '#1d1d1d';
+      document.body.style.color = 'aliceblue';
+      document.body.style.fontSize = '20px';
 
-})
+    }
+  })
